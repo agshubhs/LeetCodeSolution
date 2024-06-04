1faacf2790b83ed733b8ef4cfea2b56a92299f1b
Shubhanshu Agarwal <agarwal.shubhanshu07@gmail.com>
2024-06-04 21:05:48 +0530

    Time: 311 ms (85.22%), Space: 125.4 MB (91.82%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
@@ -8,21 +8,11 @@ public:
         int currMax = meetings[0][1];
         for(int i = 1 ; i< n;i++){
            if(currMax < meetings[i][0]){
-            cout<<i<<" ";
-                ans  = ans + meetings[i][0]- currMax-1;
-           }
+                ans  = ans + meetings[i][0] -currMax-1;
+            }
            currMax = max(currMax, meetings[i][1]);
         }
         ans = ans + days-currMax;
-        // int ans = 0;
-        // int temp = count[0];
-        // for(int i = 1;i < days;i++){
-        //     if (temp == 0)
-        //         ans++;
-        //     temp = temp + count[i];
-        // }
-        // if(temp == 0)
-        //     ans++;
         return ans;
         
     }
