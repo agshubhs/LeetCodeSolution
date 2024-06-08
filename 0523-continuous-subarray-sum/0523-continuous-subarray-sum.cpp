0d8fae25761a29dae0508ab5b85718648d4d791c
Shubhanshu Agarwal <agarwal.shubhanshu07@gmail.com>
2024-06-08 16:45:23 +0530

    Time: 243 ms (14.65%), Space: 135.2 MB (19.15%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,7 +3,7 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int sum = 0;
         int n = nums.size();
-        if(k ==0)
+        if(k == 0)
             return true;
         if(n<2)
             return false;
@@ -12,7 +12,7 @@ public:
         rem[0] = -1;
         for(int i = 0 ;i<n;i++){
             sum  += nums[i];
-            sum = sum%k;
+            sum  %= k;
             if(rem.find(sum) != rem.end()){
                 if(rem[sum] +1 < i)
                     return true;
