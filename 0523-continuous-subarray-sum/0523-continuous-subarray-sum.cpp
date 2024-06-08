12055b2a7f853acf5914763eeee5d1dca13cc95d
Shubhanshu Agarwal <agarwal.shubhanshu07@gmail.com>
2024-06-08 16:47:38 +0530

    Time: 186 ms (40.57%), Space: 134.6 MB (86.97%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -8,7 +8,7 @@ public:
         if(n<2)
             return false;
         
-        map<int, int> rem;
+        unordered_map<int, int> rem;
         rem[0] = -1;
         for(int i = 0 ;i<n;i++){
             sum  += nums[i];
